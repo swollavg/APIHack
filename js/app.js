632fe534111b55e868c087d2f0a2637a0870607a
Antony Grove <agrove86@gmail.com>
2014-09-01 23:30:36 -0500

    save
    added random listing functonality

--- js/app.js
+++ js/app.js
@@ -1,18 +1,47 @@
 $(document).ready(function(){
 	// variables
 	var categoryDress = "Clothing/Women/Dress";
-	var categoryShirt = "Clothing/Women/Shirt";
+	var categoryPants = "Clothing/Women/Pants";
 	var categoryShoes = "Clothing/Shoes/Women";
 	var categoryJewelry = "Jewelry";
-	var categoryBags = {};
-	var base = $('#dressBase');
+	var categoryBag = "Bags and Purses";
 
+	var dressBase = $('#dress-button');
+	var pantsBase = $('#pants-button');
+	var shoesBase = $('#shoes-button');
+	var jewelryBase = $('#jewelry-button');
+	var bagBase = $('#bag-button');
 
+	
 	$('#cart-wrapper').hide();
-	$('.add-cart').click(function(){
-		tester(categoryJewelry, base);
-		$('#cart').show();
+
+	// Processes Dress data
+	$(dressBase).click(function(){
+		var randomOff = Math.floor((Math.random() * 500) + 1);
+		alert(randomOff);
+		processEtsyData(categoryDress, dressBase, randomOff);
 	});
+
+	// Processes Pants data
+	$(pantsBase).click(function(){
+		processEtsyData(categoryPants, pantsBase, randomOff);
+	});
+
+	// Processes Shoes data
+	$(shoesBase).click(function(){
+		processEtsyData(categoryShoes, shoesBase, randomOff);
+	});
+
+	// Processes Jewelry data
+	$(jewelryBase).click(function(){
+		processEtsyData(categoryJewelry, jewelryBase, randomOff);
+	});
+
+	// Processes bag data
+	$(bagBase).click(function(){
+		processEtsyData(categoryBag, bagBase, randomOff);
+	});
+	
 	// Shows the shopping cart
 	$('.about a').click(function(){
 		$('.header-wrapper').slideUp(function(){
@@ -48,36 +77,39 @@ var replaceHtmlEntites = (function() {
 })();
 
 
-function tester(filter, buttonBase) {
+function processEtsyData(filter, buttonBase, off) {
 	$.ajax({
 		url: "https://openapi.etsy.com/v2/listings/active.js?api_key=6z1xqkz8tg0znk9ckgkf9p5p",
 		dataType: "jsonp",
 		data: {
 			"category": filter,
-			"limit": "7",
+			"limit": "15",
+			"offset": off,
 			"includes": "Images" + "\,User" + "\,ShippingInfo" + "\,Shop/About",   
 
 		},
 		type: "GET",
 		success: function(data){
 
+			var randomNumber = Math.floor((Math.random() * 15) + 1);
+		
 			// shows hidden overview list
 			$('.overview-list').removeClass("hide");
 
 			//sets user name and shop link
-			setShopAndName(data, buttonBase);
+			setShopAndName(data, buttonBase, randomNumber);
 
 			// sets the items picture
-			buttonBase.parent('.details').prev().css("background-image", 'url(' + data.results[1].Images[0].url_570xN + ")");
-			buttonBase.parent('.details').prev().attr('href', data.results[1].url);
+			buttonBase.parent('.details').prev().css("background-image", 'url(' + data.results[randomNumber].Images[0].url_570xN + ")");
+			buttonBase.parent('.details').prev().attr('href', data.results[randomNumber].url);
 			
 			// sets the description text and replace html special characters
-			var originalString = data.results[1].description;
+			var originalString = data.results[randomNumber].description;
 			var editString = replaceHtmlEntites(originalString);
 			buttonBase.parent('.details').find('.description').append(editString);
 
 			//sets title
-			buttonBase.parent('.details').find('.title').children('.title-descrip').text(data.results[1].title);
+			buttonBase.parent('.details').find('.title').children('.title-descrip').text(data.results[randomNumber].title);
 		
 
 			//Jquery plugin self running function that truncates
@@ -92,31 +124,31 @@ function tester(filter, buttonBase) {
 			});
 
 			// sets price
-			buttonBase.parent('.details').find('.price').children('span').append(data.results[1].price);
+			buttonBase.parent('.details').find('.price').children('span').text(data.results[randomNumber].price);
 			
 			// Sets an array for the materails
-			var materialArray = data.results[1].materials;
+			var materialArray = data.results[randomNumber].materials;
 
 			// removed comma from long array list
 		    $.each(materialArray, function(index, value){
 		    	if(index == materialArray.length - 1){
-		    		buttonBase.prev().find('.material').children('span').append(" " + value + '.');
+		    		buttonBase.prev().prev().find('.material').children('span').append(" " + value + '.');
 		    		
 		    	}
 		    	else if(index < materialArray.length - 1)
-					buttonBase.prev().find('.material').children('span').append(" " + value + ',');
+					buttonBase.prev().prev().find('.material').children('span').append(" " + value + ',');
 			});
 
 		    // sets shipping info
-		   shippingIterate(data, buttonBase);
+		   shippingIterate(data, buttonBase, randomNumber);
 
 		   // sets handmade information
-		   handmade(data, buttonBase);                  
+		   handmade(data, buttonBase, randomNumber);                  
 
 		   //sets feedback score
-		   var feedbackScore = data.results[1].User.feedback_info.count;
-		   var feedbackLink = data.results[1].Shop.shop_name;
-			buttonBase.prev().find('.score').html("Feedback: " + "<a href='" + "https://www.etsy.com/shop/" + feedbackLink + "/reviews?ref=shop_info" + "'" + "target=_blank" + ">"  + feedbackScore + " reviews" + "</a>");
+		   var feedbackScore = data.results[randomNumber].User.feedback_info.count;
+		   var feedbackLink = data.results[randomNumber].Shop.shop_name;
+			buttonBase.prev().prev().find('.score').html("Feedback: " + "<a href='" + "https://www.etsy.com/shop/" + feedbackLink + "/reviews?ref=shop_info" + "'" + "target=_blank" + ">"  + feedbackScore + " reviews" + "</a>");
 
 		},
 		error: function(jqXHR, error, errorThrown){
@@ -127,58 +159,63 @@ function tester(filter, buttonBase) {
 
 //iterates over each ship array and then each object within
 // each array looking for keywords.
-function shippingIterate(data, buttonBase){
-var shippingArray = data.results[1].ShippingInfo;
+function shippingIterate(data, buttonBase, randomNumber){
+var shippingArray = data.results[randomNumber].ShippingInfo;
 
 	$.each(shippingArray, function(index, value){
+		var notFound = true;
 		$.each(value, function(key, value){
 			
 			if(value == "Everywhere Else"){
-				buttonBase.prev().find('.shipping').children('.shipto').text('worldwide');
-				return false;
+				buttonBase.prev().prev().find('.shipping').children('.shipto').text('worldwide');
+			    notFound = false;
+				return notFound;
 			}
 
 			else if(key == "destination_country_name" && value != "Everywhere Else"){
 				if(shippingArray.length <= 8){
-					buttonBase.prev().find('.shipping').children('.shipto').append("to " + data.results[1].ShippingInfo[index].destination_country_name);
+					buttonBase.prev().prev().find('.shipping').children('.shipto').append("to " + data.results[randomNumber].ShippingInfo[index].destination_country_name);
+					notFound == true;
 				}
 			}
 
-			buttonBase.prev().find('.shipping').children('.shipfrom').text(data.results[1].ShippingInfo[index].origin_country_name);							
-		});
-		return false
+			buttonBase.prev().prev().find('.shipping').children('.shipfrom').text(data.results[randomNumber].ShippingInfo[index].origin_country_name);	
+									
+		}); 
+		return notFound;
+		
 	});
 }
 
 
-function handmade(data, buttonBase){
-	var handmadeInfo = data.results[1].who_made;
-	var supplyInfo = data.results[1].is_supply;
+function handmade(data, buttonBase, randomNumber){
+	var handmadeInfo = data.results[randomNumber].who_made;
+	var supplyInfo = data.results[randomNumber].is_supply;
 
 	if(handmadeInfo == "i_did" || handmadeInfo == "collective") {
-		buttonBase.prev().find('.handmade').text('Handmade');
+		buttonBase.prev().prev().find('.handmade').text('Handmade');
 
 		if(supplyInfo == "true"){
-			buttonBase.prev().find('.handmade').append(" " + "supply");
+			buttonBase.prev().prev().find('.handmade').append(" " + "supply");
 		}
 
 		else if(supplyInfo == "false"){
-			buttonBase.prev().find('.handmade').append(" " + "item");
+			buttonBase.prev().prev().find('.handmade').append(" " + "item");
 		}
 	} 
 
 	else {
-		buttonBase.prev().find('.handmade').hide();
+		buttonBase.prev().prev().find('.handmade').hide();
 	}
 }
 
-function setShopAndName(data, buttonBase) {
+function setShopAndName(data, buttonBase, randomNumber) {
 	// sets the sellers name
-	buttonBase.prev().find('.seller-info').children('a').text(data.results[1].User.login_name);
-	console.log(data.results[1]);
+	buttonBase.prev().prev().find('.seller-info').children('a').text(data.results[randomNumber].User.login_name);
+	console.log(data.results);
 
 	//sets link to user shop
-	var shopLink = data.results[1].Shop.url;
-	buttonBase.prev().find('.seller-info').children('a').attr('href', shopLink);
+	var shopLink = data.results[randomNumber].Shop.url;
+	buttonBase.prev().prev().find('.seller-info').children('a').attr('href', shopLink);
 }
 
